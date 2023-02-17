6bb325a2b470c4fa7a6b7a6631c24b7a62d9fda7
Section1 <otinoco@digiocio.com>
2023-02-17 10:45:47 +0100

    CODI 2 - Configurar la estructura de la App

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"log"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("Hello World")
+//Crearem un struct amb totes les configuracions que necessiti la nostre App
+type Config struct {
+	App fyne.App //Definim que emprara Fyne per construir la GUI de l'App
+	InfoLog *log.Logger //Definim un Log d'accions
+	ErrorLog *log.Logger //Definim un Log d'errors
+}
 
-	w.SetContent(widget.NewLabel("Hello World!"))
-	w.ShowAndRun()
+var myApp Config //Creem una variable que sigui de tipus Config i aixi enmagatzemar la configuració de l'App
+
+func main() {
+	// crearem una aplicació fyne
+
+	//crearem els nostres logs
+
+	//conexió amb la base de dades
+
+	//crearem un repositori de base de dades
+
+	//crearem i definim el tamany de una pantalla de fyne
+
+	//mostrar i executar l'aplicació
 }
