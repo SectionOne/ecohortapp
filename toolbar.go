c72366953cc68e17ffbe734901ea5a177692271a
Section1 <otinoco@digiocio.com>
2023-02-17 14:08:51 +0100

    CODI 39 - Validem les dades del formulari

--- toolbar.go
+++ toolbar.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"ecohortapp/repository"
+	"strconv"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
@@ -37,6 +41,29 @@ func (app *Config) addRegistresDialog() dialog.Dialog {
 	app.AfegirRegistresTempMaximaEntrada = tempMinimaEntrada
 	app.AfegirRegistresHumitatEntrada = humitatEntrada
 
+	validacioData := func(s string) error {
+		//Apliquem el format de la data definit en el primer parametre al string indicat en el segon parametre i que correspon el aportat per l'usuari
+		//S’ha de referenciar el format amb el layout standard 2006-01-02 i no pas amb cualsevol data
+		if _, err := time.Parse("2006-01-02", s); err != nil {
+			//Si es produeix algun error en el procés el retornem
+			return err
+		}
+		return nil
+	}
+	dataRegistreEntrada.Validator = validacioData
+
+	esIntValidador := func(s string) error {
+		_, err := strconv.Atoi(s)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	precipitacioEntrada.Validator = esIntValidador
+	tempMaximaEntrada.Validator = esIntValidador
+	tempMinimaEntrada.Validator = esIntValidador
+	humitatEntrada.Validator = esIntValidador
+
 	//Definim un placeholder i aixi facilitar l'usabilitat en el camp data de compra
 	dataRegistreEntrada.PlaceHolder = "YYYY-MM-DD"
 
@@ -56,7 +83,28 @@ func (app *Config) addRegistresDialog() dialog.Dialog {
 		//A continuació realitzem la validació de les dades
 		func(valid bool) {
 			if valid {
-				//TODO: Desenvolupar un filtratge
+				//Desenvolupar un filtratge, convertint les dades a els formats de la bd
+				//S’ha de referenciar el format amb el layout standard 2006-01-02 i no pas amb cualsevol data
+				dataRegistre, _ := time.Parse("2006-01-02", dataRegistreEntrada.Text)
+				precipitacio, _ := strconv.Atoi(precipitacioEntrada.Text)
+				tempMaxima, _ := strconv.Atoi(tempMaximaEntrada.Text)
+				tempMinima, _ := strconv.Atoi(tempMinimaEntrada.Text)
+				humitat, _ := strconv.Atoi(humitatEntrada.Text)
+				
+				//Invoquem el mètode de la base de dades per insertar registres i que poblarem amb les dades formatejades
+				_, err := app.DB.InsertRegistre(repository.Registres{
+					Data:        dataRegistre,
+					Precipitacio:  precipitacio,
+					TempMaxima: tempMaxima,
+					TempMinima: tempMinima,
+					Humitat: humitat,
+				})
+				//Controlem si és produeix algun error
+				if err != nil {
+					app.ErrorLog.Println(err)
+				}
+				//Invoquem el paremetre del struct Config per permetre que refresqui el widget de la taula amb el nou registre
+				app.actualitzarRegistresTable()
 			}
 		},
 		app.MainWindow)
