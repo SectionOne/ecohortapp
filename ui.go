1314886a9a1417110517d3ac165d8dfd4058653d
Section1 <otinoco@digiocio.com>
2023-02-17 10:58:37 +0100

    CODI 10 - Insertem el widget del clima en un conte

--- ui.go
+++ ui.go
@@ -1,9 +1,16 @@
 package main
 
+import "fyne.io/fyne/v2/container"
+
 func (app *Config) makeUI() {
 	// obtenir les dades de l'API (Probabilitat de precipitacions, Temperatura Max. i Min. i Humitat)
 	precipitacio, tempMax, tempMin, humitat := app.getClimaText()
 	//insertar la informació dins del contenidor
-
+	climaDadesContent := container.NewGridWithColumns(4,
+		precipitacio,
+		tempMax,
+		tempMin,
+		humitat,
+	)
 	//afegir el contenidor a la finestra
 }
