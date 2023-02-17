60bb0395c61f0dfd164773524377952817607908
Section1 <otinoco@digiocio.com>
2023-02-17 13:00:42 +0100

    CODI 29 - Connectant la nostre App a SQLITE

--- go.mod
+++ go.mod
@@ -2,15 +2,16 @@ module ecohortapp
 
 go 1.19
 
-require fyne.io/fyne/v2 v2.3.0
+require (
+	fyne.io/fyne/v2 v2.3.0
+	github.com/glebarez/go-sqlite v1.20.3
+)
 
 require (
 	fyne.io/systray v1.10.1-0.20221115204952-d16a6177e6f1 // indirect
 	github.com/benoitkugler/textlayout v0.3.0 // indirect
-	github.com/chromedp/cdproto v0.0.0-20230208104036-c3c870aa4771 // indirect
-	github.com/chromedp/chromedp v0.8.7 // indirect
-	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/fredbi/uri v0.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
@@ -19,16 +20,14 @@ require (
 	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
 	github.com/go-text/typesetting v0.0.0-20221212183139-1eb938670a1f // indirect
-	github.com/gobwas/httphead v0.1.0 // indirect
-	github.com/gobwas/pool v0.2.1 // indirect
-	github.com/gobwas/ws v1.1.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/goki/freetype v0.0.0-20220119013949-7a161fd3728c // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
-	github.com/josharian/intern v1.0.0 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
-	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230126093431-47fa9a501578 // indirect
 	github.com/srwiley/oksvg v0.0.0-20220731023508-a61f04f16b76 // indirect
 	github.com/srwiley/rasterx v0.0.0-20210519020934-456a8d69b780 // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
@@ -37,8 +36,12 @@ require (
 	golang.org/x/image v0.0.0-20220601225756-64ec528b34cd // indirect
 	golang.org/x/mobile v0.0.0-20211207041440-4e6c2922fdee // indirect
 	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
-	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
+	modernc.org/libc v1.22.2 // indirect
+	modernc.org/mathutil v1.5.0 // indirect
+	modernc.org/memory v1.5.0 // indirect
+	modernc.org/sqlite v1.20.3 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"ecohortapp/repository"
 	"log"
 	"net/http"
 	"os"
@@ -8,22 +10,24 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	_ "fyne.io/fyne/v2/widget"
+	_ "github.com/glebarez/go-sqlite"
 )
 
 // Crearem un struct amb totes les configuracions que necessiti la nostre App
 type Config struct {
-	App                      fyne.App        //Definim que emprara Fyne per construir la GUI de l'App
-	InfoLog                  *log.Logger     //Definim un Log d'accions
-	ErrorLog                 *log.Logger     //Definim un Log d'errors
-	MainWindow               fyne.Window     //Aqui enmagatzemem la referencia a certes arees de la ui per controlar les actualitzacions de les mateixes.
-	ClimaDadesContainer      *fyne.Container //Guardem el contenidor de les dades del clima, referenciant el punter de memòria del contenidor de fyne.
-	PronosticGraficContainer *fyne.Container //Definim un camp a on enmagatzem el contenidor del gràfic de clima, que ara sera de tipus contenidor fyne
-	HTTPClient               http.Client     //Afegim la referència al client http sence necessitat de invocar la llibreria
+	App                      fyne.App              //Definim que emprara Fyne per construir la GUI de l'App
+	InfoLog                  *log.Logger           //Definim un Log d'accions
+	ErrorLog                 *log.Logger           //Definim un Log d'errors
+	DB                       repository.Repository //Definim la referencia a la conexió a SQLite
+	MainWindow               fyne.Window           //Aqui enmagatzemem la referencia a certes arees de la ui per controlar les actualitzacions de les mateixes.
+	ClimaDadesContainer      *fyne.Container       //Guardem el contenidor de les dades del clima, referenciant el punter de memòria del contenidor de fyne.
+	PronosticGraficContainer *fyne.Container       //Definim un camp a on enmagatzem el contenidor del gràfic de clima, que ara sera de tipus contenidor fyne
+	HTTPClient               http.Client           //Afegim la referència al client http sence necessitat de invocar la llibreria
 }
 
-var myApp Config //Creem una variable que sigui de tipus Config i aixi enmagatzemar la configuració de l'App
-
 func main() {
+	var myApp Config //Creem una variable que sigui de tipus Config i aixi enmagatzemar la configuració de l'App
+
 	// crearem una aplicació fyne
 	fyneApp := app.NewWithID("cat.cibernarium.ecohortapp") //El definit el mètode New amb una id ens permet distribuir la nostre app en un MarketPlace
 	myApp.App = fyneApp
@@ -33,8 +37,14 @@ func main() {
 	myApp.ErrorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Lshortfile) //Creraem un log per els registres d'error
 
 	//conexió amb la base de dades
+	sqlDB, err := myApp.connectSQL() //Invoquem la funció d'establiment de la conexió
+	if err != nil {
+		//Recordem que Panic es l'equivalent a Print pero acompanyat a una crida a Panic
+		log.Panic(err)
+	}
 
 	//crearem un repositori de base de dades
+	myApp.setupDB(sqlDB)
 
 	//crearem i definim el tamany de una pantalla de fyne
 	myApp.MainWindow = fyneApp.NewWindow("Eco Hort App")
@@ -46,4 +56,39 @@ func main() {
 
 	//mostrar i executar l'aplicació
 	myApp.MainWindow.ShowAndRun()
+}
+
+// Realitzarem una funció per invocar la conexió a la BD
+func (app *Config) connectSQL() (*sql.DB, error) {
+	path := ""
+
+	//Treballarem amb variables d'entorn per establir les configuracions i en aquest cas comprobem si DB_PATH (que fa referencia a la ruta de la db) te un valor o no.
+	if os.Getenv("DB_PATH") != "" {
+		//En cas de tenir valor, el recupera
+		path = os.Getenv("DB_PATH")
+	} else {
+		//En cas contrari crearà aquest arxiu de Bases de dades dins de la ruta de l'aplicació
+		path = app.App.Storage().RootURI().Path() + "/sql.db"
+		//Incloem un registre de control al log
+		app.InfoLog.Println("db in:", path)
+	}
+
+	//A continuació establim la conexió i controlem possibles errors
+	db, err := sql.Open("sqlite", path)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// Creem un repositori per a la Base de Dades
+func (app *Config) setupDB(sqlDB *sql.DB) {
+	app.DB = repository.NewSQLiteRepository(sqlDB)
+
+	err := app.DB.Migrate()
+	if err != nil {
+		app.ErrorLog.Println(err)
+		log.Panic()
+	}
 }
